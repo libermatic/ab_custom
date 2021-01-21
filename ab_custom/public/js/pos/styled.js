67bc0c793cd7e256851d1de8aa013799cf767706
Sun Howwrongbum <sun@libermatic.com>
2021-01-21 11:29:58 +0000

    fix(pos): items as list

--- ab_custom/public/js/pos/styled.js
+++ ab_custom/public/js/pos/styled.js
@@ -17,11 +17,36 @@ export function styledItems(Items) {
   return makeExtension(
     'styledItems',
     class ItemsWithStyled extends Items {
+      make_dom() {
+        super.make_dom();
+        this.items_wrapper.find('.pos-items').addClass('list-group');
+      }
       render_items(items) {
         const all_items = Object.values(items || this.items).map((item) =>
           this.get_item_html(item)
         );
         this.clusterize.update(all_items);
+      }
+      get_item_html({
+        item_code,
+        item_name,
+        price_list_rate,
+        actual_qty,
+        stock_uom,
+      }) {
+        return `
+          <a class="pos-item-wrapper list-group-item list-group-item-action" data-item-code="${escape(
+            item_code
+          )}">
+            <div>
+              <div>${item_name || item_code}</div>
+              <div class="small">
+                ${item_code} · ${actual_qty || 0} ${stock_uom || ''}
+              </div>
+            </div>
+            <div>${format_currency(price_list_rate, this.currency)}</div>
+          </a>
+        `;
       }
     }
   );
