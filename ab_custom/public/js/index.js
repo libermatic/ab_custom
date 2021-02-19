13c307971ba7f6de9b28a7f60c1397cf29c34ceb
Sun Howwrongbum <sun@libermatic.com>
2021-02-19 11:29:42 +0530

    0.0.7

--- ab_custom/public/js/index.js
+++ ab_custom/public/js/index.js
@@ -1,7 +1,7 @@
 import * as reports from './reports';
 import * as extensions from './extensions';
 
-const __version__ = '0.0.6';
+const __version__ = '0.0.7';
 
 frappe.provide('ab_custom');
 ab_custom = { __version__, reports, extensions };
