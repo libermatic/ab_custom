e6d90da137189a6117a44d6a448208a47d3bc8db
Sun Howwrongbum <sun@libermatic.com>
2021-02-25 16:36:17 +0000

    feat(report): detail columns

--- ab_custom/ab_custom/report/simple_sales_register/simple_sales_register.py
+++ ab_custom/ab_custom/report/simple_sales_register/simple_sales_register.py
@@ -6,17 +6,30 @@ import frappe
 from erpnext.accounts.report.sales_register.sales_register import (
     execute as sales_register,
 )
-from toolz.curried import compose, concatv, excepts, first, keyfilter, merge
-from toolz.itertoolz import groupby
+from toolz.curried import (
+    compose,
+    concatv,
+    excepts,
+    first,
+    keyfilter,
+    merge,
+    groupby,
+    valmap,
+    first,
+)
 
 
 def execute(filters=None):
     result = sales_register(filters)
     invoices = [x.get("invoice") for x in result[1]]
     payments, mops = _get_payments(invoices)
+    more_details = _get_more_details(invoices)
 
     return concatv(
-        (_get_columns(result[0], mops), _get_data(result[1], payments, mops)),
+        (
+            _get_columns(result[0], mops),
+            _get_data(result[1], payments, mops, more_details),
+        ),
         result[2:],
     )
 
@@ -24,8 +37,9 @@ def execute(filters=None):
 WHITELISTED_COLUMNS = [
     "invoice",
     "posting_date",
+    "posting_time",
     "customer",
-    "grand_total",
+    "total_qty",
     "rounded_total",
     "outstanding_amount",
 ]
@@ -40,7 +54,26 @@ def _get_columns(columns, mops):
         lambda x: filter(lambda col: col.get("fieldname") == x, columns),
     )
 
-    def make_columns(mop):
+    def make_column(fieldname):
+        if fieldname == "posting_time":
+            return {
+                "fieldname": fieldname,
+                "fieldtype": "Time",
+                "label": "Posting Time",
+                "width": 120,
+            }
+
+        if fieldname == "total_qty":
+            return {
+                "fieldname": fieldname,
+                "fieldtype": "Float",
+                "label": "Total Qty",
+                "width": 90,
+            }
+
+        return pick_column(fieldname)
+
+    def make_mop_column(mop):
         return {
             "fieldname": mop,
             "fieldtype": "Currency",
@@ -49,12 +82,12 @@ def _get_columns(columns, mops):
             "width": 120,
         }
 
-    return [pick_column(x) for x in WHITELISTED_COLUMNS] + [
-        make_columns(x) for x in sorted(mops)
+    return [make_column(x) for x in WHITELISTED_COLUMNS] + [
+        make_mop_column(x) for x in sorted(mops)
     ]
 
 
-def _get_data(data, payments, mops):
+def _get_data(data, payments, mops, details):
     includes = WHITELISTED_COLUMNS + ["customer_name"]
     payments_by_invoice = groupby("invoice", payments)
 
@@ -72,7 +105,11 @@ def _get_data(data, payments, mops):
         }
 
     return [
-        merge(keyfilter(lambda x: x in includes, row), get_payments(row))
+        merge(
+            details.get(row.get("invoice")) or {},
+            keyfilter(lambda x: x in includes, row),
+            get_payments(row),
+        )
         for row in data
     ]
 
@@ -103,3 +140,21 @@ def _get_payments(invoices):
     )
     mops = set([x.get("mode_of_payment") for x in payments])
     return payments, mops
+
+
+def _get_more_details(invoices):
+    if not invoices:
+        return []
+
+    return valmap(
+        first,
+        groupby(
+            "invoice",
+            frappe.get_all(
+                "Sales Invoice",
+                filters={"name": ("in", invoices)},
+                fields=["name as invoice", "posting_time", "total_qty"],
+            ),
+        ),
+    )
+
