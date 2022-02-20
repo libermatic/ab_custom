371cae6e78fd8b320c98734a728289ed0dedd55c
Sun Howwrongbum <sun@libermatic.com>
2022-02-20 17:14:00 +0530

    fix(pos): cart index not refreshing on remove

--- ab_custom/public/js/pos/item_idx.js
+++ ab_custom/public/js/pos/item_idx.js
@@ -4,6 +4,14 @@ export default function item_idx(ItemCart) {
   return makeExtension(
     'item_idx',
     class ItemCartWithItemIdx extends ItemCart {
+      update_item_html(item, remove_item) {
+        super.update_item_html(item, remove_item);
+        if (remove_item) {
+          this.$cart_items_wrapper.find('.cart-item-wrapper > .item-serial').each(function(index) {
+            $(this).text(index + 1);
+          });
+        }
+      }
       render_cart_item(item_data, $item_to_update) {
         super.render_cart_item(item_data, $item_to_update);
         this.get_cart_item(item_data).find('.item-image').replaceWith(`
